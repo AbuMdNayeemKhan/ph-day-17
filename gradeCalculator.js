1797bfd13467758b973404a43790628903972faa
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-01-29 00:34:14 +0600

    gradeCalculator done

--- gradeCalculator.js
+++ gradeCalculator.js
@@ -11,3 +11,16 @@ Create a simple JavaScript program that takes a student's score as input and ret
     F: 0-59
 
 ***/
+
+let marks = 92;
+if (marks >= 90) {
+    console.log("A");
+} else if (marks >= 80) {
+    console.log("B");
+} else if (marks >= 70){
+    console.log("C");
+} else if (marks >= 60){
+    console.log("D");
+} else {
+    console.log("F");
+};
