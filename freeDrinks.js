187db2acd1ac6ffb6339ab96a7adfa43c1f952c0
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-01-28 20:39:25 +0600

    free drinks done

--- freeDrinks.js
+++ freeDrinks.js
@@ -1,6 +1,14 @@
 /***
-
 Free Drinks
     - Burger more than 500tk: free Coke
     - Else Coke: 30tk
 */
+
+const burgerPrice = 700;
+const cokePrice = 30;
+
+if (burgerPrice > 500) {
+  console.log("your total cost is: "+burgerPrice+". and you won a coke free.");
+} else {
+  console.log("Burger price is: "+burgerPrice+". cock Price is: "+cokePrice+". Your total price is: "+(burgerPrice + cokePrice));
+}
