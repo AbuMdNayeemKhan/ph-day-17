73b9e3f94d6f16eb63da0b6774d5c0b93b169823
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-01-28 23:50:11 +0600

    but fare calculator added

--- busFareCalculator.js
+++ busFareCalculator.js
@@ -6,3 +6,17 @@ Ticket fare Calculator
     - Senior citizens (age >= 60) gets a 15% Discount
     - Otherwise Regular ticket fare 800 tk
 */
+
+let ticketPrice = 10;
+let age = 40;
+let isStudent = true;
+
+if(age < 10){
+    console.log("Yahoo! you'r free.");
+} else if (isStudent == true){
+    console.log("You have 50% dicount. Please give me: "+ ticketPrice / 100 * 50 +"taka");
+} else if (age <= 60){
+    console.log("You have 15% dicount. Please give me: "+ ticketPrice / 100 * 15 +"taka");
+} else{
+    console.log("Please give me: "+ ticketPrice / 100 * 15 +"taka");
+}
